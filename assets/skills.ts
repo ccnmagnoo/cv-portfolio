cc5e0af05ca92a06afe929fd57da372a99d8fec6
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-03-04 17:27:15 -0300

    asset skills

---
+++ assets/skills.ts
@@ -0,0 +1,28 @@
+import { Skill, SkillLevel } from '../model/Tech';
+
+export const skills: Skill[] = [
+  /*Languages*/
+  new Skill(SkillLevel.expert, 'Typescript', 'Javascript', 'TS', 'typescript-icon'),
+  new Skill(SkillLevel.skilled, 'Python', 'Numpy/Pandas', 'Py', 'python'),
+  new Skill(SkillLevel.average, 'Kotlin', 'Java/Android', 'KT', 'kotlin'),
+  /*Fron-end*/
+  new Skill(SkillLevel.skilled, 'ReacJs', 'Hooks', 'Rjs', 'file-type-reactjs'),
+  new Skill(SkillLevel.average, 'NextJs', 'React FrameWork', 'Njs', 'nextjs-icon'),
+  /*Databases*/
+  new Skill(SkillLevel.specialist, 'Firebase', 'Firestore/NoSQL', 'Fs', 'firebase'),
+  new Skill(
+    SkillLevel.average,
+    'MongoDb',
+    'Mongoose/NoSQL',
+    'Mdb',
+    'folder-type-mongodb'
+  ),
+  /*HTML*/
+  new Skill(SkillLevel.basic, 'PostgreSQL', 'SQL', 'Psql', 'postgresql'),
+  new Skill(SkillLevel.specialist, 'MaterialUI', 'UI', 'Mui', 'material-ui'),
+  new Skill(SkillLevel.average, 'Bootstrap', 'UI', 'Bs', 'bootstrap'),
+  new Skill(SkillLevel.specialist, 'HTML5', 'Web', 'Html', 'file-type-html'),
+  new Skill(SkillLevel.skilled, 'CSS3', 'Web', 'CSS', 'file-type-css'),
+  /*others*/
+  new Skill(SkillLevel.basic, 'Git', 'GitHub', 'Git', 'git-icon'),
+];
