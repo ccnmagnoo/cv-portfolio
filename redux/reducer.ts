c652353ec98f1117147bf7d1807234a8465ec542
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-03-04 17:27:31 -0300

    refactor appstate

--- redux/reducer.ts
+++ redux/reducer.ts
@@ -1,21 +1,7 @@
 //configuration
 
-import { Interest, Skill } from '../model/Tech';
+import { AppState } from '../model/AppState.interface';
 
-export interface AppState {
-  basicData: {
-    name: string;
-    profesion: string;
-    description?: string /*no more than 3 lines*/;
-  };
-
-  skills?: Skill[];
-  interest?: Interest[];
-  hobbies?: { name: string; description: string; url: string }[];
-  curriculum: [];
-  social: [];
-  certifications: [];
-}
 //init
 export const initialState: Partial<AppState> = {
   basicData: { name: 'Carlos Campos', profesion: 'Mid Developer / Mechanical Engineer' },
