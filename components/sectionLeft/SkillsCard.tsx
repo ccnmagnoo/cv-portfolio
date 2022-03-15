16931be41eabd384f52feeb046c13eb19f55cfdc
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-03-15 15:01:46 -0300

    titles children

--- components/sectionLeft/SkillsCard.tsx
+++ components/sectionLeft/SkillsCard.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { AppContext } from '../../redux/context';
-import { Meter } from '../meter/Meter';
 import { TechCard } from './TechCard';
-
 export const SkillsCard = () => {
   const context = React.useContext(AppContext);
   const skillList = context.skills;
@@ -12,10 +10,10 @@ export const SkillsCard = () => {
         <h6>Relative skill set</h6>
       </TechCard>
       <TechCard filter={'front-end'} techList={skillList}>
-        <h6>front-end</h6>
+        <h6>Front-end</h6>
       </TechCard>
       <TechCard filter={'back-end'} techList={skillList}>
-        <h6>back-end</h6>
+        <h6>Back-end</h6>
       </TechCard>
       <TechCard filter={'data-analytics'} techList={skillList}>
         <h6>Data Analytics</h6>
