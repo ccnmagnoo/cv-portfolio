324d1d9ce6d4b59a2af60e6490186c5f46d1d261
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-03-18 10:51:21 -0300

    implementing comments

--- assets/skills.ts
+++ assets/skills.ts
@@ -9,7 +9,7 @@ const skills: Skill[] = [
     'TS',
     'logos/typescript-icon',
     new Date(2022, 2, 1),
-    'Current favorite language. I prefer it over JS and a blame Kotlin for that. It is familiar & comfortable for me, and his lint saved me a lot of debuggin`s hours.'
+    "Current favorite language 🥇, a stronly type programing makes me feel more secure and comfortable with services I'm giving. Simply the best"
   ),
   new Skill(
     Level.skilled,
@@ -18,7 +18,7 @@ const skills: Skill[] = [
     'KT',
     'logos/kotlin',
     new Date(2019, 6, 6),
-    'Second language, I learned this over Java because his lovely resemblance to Python. I started into POO world and hard typing thanks to this.'
+    'This lang was the second I learned, and basically teach me all a know about POO. Beautiful 🌞 Python alike sintax.'
   ),
   new Skill(
     Level.average,
@@ -27,7 +27,7 @@ const skills: Skill[] = [
     'Py',
     'logos/python',
     new Date(2017, 3, 1),
-    "Python's my first love, opened my eyes when i needed to evolve from VBA💘, helping me to solve a lot of engineering problems giving me a powefull tools."
+    'Python will be always my first love 💘. His kwnolegde gave me a huge boost in my capabilities to solving engineering problems and data analisys.'
   ),
   new Skill(
     Level.basic,
@@ -36,7 +36,7 @@ const skills: Skill[] = [
     'Git',
     'logos/git-icon',
     new Date(2020, 7, 13),
-    'Necesary basic skill, I know how to use just basics; you know push pull, fork & commit'
+    'A "Must Need" tool. I use it in all my proyects, and know pretty well his basics features like: push, pull, fork and rebase.'
   ),
   /*Fron-end*/
   new Skill(
@@ -45,16 +45,18 @@ const skills: Skill[] = [
     'front-end',
     'Html',
     'vscode-icons/file-type-html',
-    new Date(2022, 1, 6)
+    new Date(2022, 1, 6),
+    'I love HTML5 and im really happy write in pure CSS3, constantly learning and creating for make software a nice experience for clients.'
   ),
   // new Skill(SkillLevel.skilled, 'CSS3', 'front-end', 'CSS', 'vscode-icons/file-type-css'),
   new Skill(
     Level.specialist,
-    'ReacJs',
+    'React Js',
     'front-end',
     'Rjs',
     'vscode-icons/file-type-reactjs',
-    new Date(2021, 12, 17)
+    new Date(2021, 12, 17),
+    'My principal front-end library. React/Hooks + TS is a indispensable tool for my solutions.'
   ),
   new Skill(
     Level.average,
@@ -62,11 +64,26 @@ const skills: Skill[] = [
     'front-end',
     'Njs',
     'logos/nextjs-icon',
-    new Date(2022, 2, 1)
+    new Date(2022, 2, 1),
+    'Well, this page is made in NextJs, and is totally awesome Framework 👍. Count with this if your need quick, secure a solid SPA'
   ),
   /*HTML*/
-  new Skill(Level.specialist, 'MaterialUI', 'front-end', 'Mui', 'logos/material-ui'),
-  new Skill(Level.average, 'Bootstrap', 'front-end', 'Bs', 'logos/bootstrap'),
+  new Skill(
+    Level.specialist,
+    'MaterialUI',
+    'front-end',
+    'Mui',
+    'logos/material-ui',
+    new Date(2021, 6, 31)
+  ),
+  new Skill(
+    Level.average,
+    'Bootstrap',
+    'front-end',
+    'Bs',
+    'logos/bootstrap',
+    new Date(2021, 3, 31)
+  ),
   /*Backend*/
   new Skill(Level.specialist, 'Firebase', 'back-end', 'Fs', 'logos/firebase'),
   new Skill(
