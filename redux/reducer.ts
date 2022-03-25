0e8db55f1a33ff25c43afc3de067905ea7dc0982
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-03-25 12:22:46 -0300

    refactor initial app state

--- redux/reducer.ts
+++ redux/reducer.ts
@@ -5,17 +5,6 @@ import skills from '../assets/skills';
 import { AppState } from '../model/AppState.interface';
 
 //init
-export const initialState: Partial<AppState> = {
-  appMode: 'dark' /*initial app mode 🌞/🌙*/,
-  basicData: {
-    name: ['cARlos', 'R.', 'cAmpos', 'N.'],
-    profesion: '<Mid Developer/> Mechanical Engineer & Data Analytics',
-  },
-  skills: skills,
-  interest: interest,
-  changeToDarkMode: () => {},
-  changeToLightMode: () => {},
-};
 
 //Actions Types
 enum ActionType {
