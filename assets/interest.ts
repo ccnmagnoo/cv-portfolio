f5e283d443eacc0c17cb3ebe2fb7b77a88806904
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-04-14 10:09:04 -0400

    interest planel addon

--- assets/interest.ts
+++ assets/interest.ts
@@ -1,8 +1,8 @@
 import { Interest, InterestLevel as Level } from '../model/Tech';
 
 const interest: Interest[] = [
-  new Interest(Level.extreme, 'GraphQl', 'back-end', 'Gql', 'logos/graphql'),
-  new Interest(Level.high, 'Prisma', 'back-end', 'Prisma', 'file-icons/prisma'),
+  new Interest(Level.moderate, 'GraphQl', 'back-end', 'Gql', 'logos/graphql'),
+  new Interest(Level.extreme, 'Prisma', 'back-end', 'Prisma', 'file-icons/prisma'),
   new Interest(Level.moderate, 'Go Lang', 'back-end', 'Go', 'vscode-icons/file-type-go'),
   new Interest(
     Level.moderate,
@@ -19,7 +19,7 @@ const interest: Interest[] = [
     'tabler/brand-react-native'
   ),
   new Interest(Level.slightly, 'Ionic', 'front-end', 'Ionic', 'logos/ionic-icon'),
-  new Interest(Level.extreme, 'Electron', 'front-end', 'Electron', 'logo/electron'),
+  new Interest(Level.high, 'Electron', 'front-end', 'Electron', 'logos/electron'),
   new Interest(
     Level.high,
     'Solidity',
--- components/sectionRight/InterestPanel.tsx
+++ components/sectionRight/InterestPanel.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { AppContext } from '../../redux/context';
+import splitter from '../../utils/splitter';
+import { TechCard } from '../sectionLeft/TechCard';
 
 export const InterestPanel = () => {
-  return <div>Interest</div>;
+  const { interest } = React.useContext(AppContext);
+  const splitted = splitter(
+    interest?.sort((a, b) => (a.interest - b.interest > 0 ? -1 : 1)),
+    3
+  );
+
+  return (
+    <article className='interest'>
+      <h6>Current Interest</h6>
+      {splitted?.map((e, i) => {
+        return <TechCard key={i} techList={e} position='right' />;
+      })}
+    </article>
+  );
 };
