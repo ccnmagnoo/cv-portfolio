9608a6c08febfc0732064bcad1e39ca7dc22fb71
Carlos Campos <crcamposn@gmail.com>
2023-01-06 00:24:54 +0000

    Update interest.ts and InterestPanel.tsx

--- components/sectionRight/InterestPanel.tsx
+++ components/sectionRight/InterestPanel.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { AppContext } from '../../redux/context';
-import splitter from '../../utils/splitter';
-import { TechCard } from '../sectionLeft/TechCard';
+import React from "react";
+import { AppContext } from "../../redux/context";
+import splitter from "../../utils/splitter";
+import { TechCard } from "../sectionLeft/TechCard";
 
 export const InterestPanel = () => {
   const { interest } = React.useContext(AppContext);
-  const splitted = splitter(
-    interest?.sort((a, b) => (a.interest - b.interest > 0 ? -1 : 1)),
-    3
-  );
+  const splitted = splitter(interest, 2);
 
   return (
     <>
       <header>
         <h6>Current Interest</h6>
       </header>
-      <article className='interestPanel'>
+      <article className="interestPanel">
         {splitted?.map((e, i) => {
-          return <TechCard key={i} techList={e} position='right' />;
+          return <TechCard key={i} techList={e} position="right" />;
         })}
       </article>
     </>
