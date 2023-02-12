f3136c58d7d3cc4f0e18cd4501f3e2fe35baadfb
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2023-02-12 12:07:06 -0300

    erase meterBar div class

--- components/meter/Meter.tsx
+++ components/meter/Meter.tsx
@@ -81,9 +81,7 @@ export const Meter = (props: Partial<Props>) => {
         {/*tech headers 🎩*/}
         <img src={props.element?.logo} alt='' /> {props.element?.tech}
         {/*colored meter bar 📶 */}
-        <div className='meterBox'>
         {getMeterBar(props.element)}
-        </div>
         {comments(props.element)}
       </MeterContainer>
 
